bcc7c6fb32ff1b3c0c9815789056daaafe571d46
talqadi7 <taq.alqadi@gmail.com>
2023-05-30 16:00:21 -0600

    fixing prompt to ensure a more targeted cover letter

--- src/query_data.py
+++ src/query_data.py
@@ -7,13 +7,14 @@ template = """You're a cover letter writer expert.
 You are given a company name, position, job description, and resume information.
 
 Please follow the following instructions at all times:
-1. DON'T make up any information or an answer.
-2. Any information should be coming directly from my resume or my cover letters.
-3. You don't have to tackle every point in the job description.
-4. Focus on my most recent experience.
-5. If a skill is not listed in my resume or cover letter, then I don't have experience in it.
+1. FOCUS ON THE JOB DESCRIPTION!
+2. DON'T make up any information or an answer.
+3. Any information should be coming directly from my resume or my cover letters.
+4. You don't have to tackle every point in the job description.
+5. Focus on my most recent experience.
+6. If a skill is not listed in my resume or cover letter, then I don't have experience in it.
 
-Write me professional and targeted cover letter for a {question}.
+Focusing on the job description: write me professional and targeted cover letter for a {question}.
 =========
 {context}
 =========
@@ -23,7 +24,7 @@ QA_PROMPT = PromptTemplate(template=template, input_variables=["context", "quest
 
 def get_chain(vectorstore):
     llm = OpenAI(
-        streaming=True, temperature=0.0, max_tokens=600, model_name="text-davinci-003"
+        streaming=True, temperature=0.05, max_tokens=600, model_name="text-davinci-003"
     )
     chain_type_kwargs = {"prompt": QA_PROMPT}
     qa_chain = RetrievalQA.from_chain_type(
