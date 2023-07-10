daf846a29ae9ef0c938c828d2755821d6a7179d5
talqadi7 <taq.alqadi@gmail.com>
2023-07-10 16:13:12 -0400

    moving non http related tasks to services

--- src/routes.py
+++ src/routes.py
@@ -6,73 +6,63 @@ from flask import (
     Response,
     stream_with_context,
     jsonify,
-    send_file
+    send_file,
 )
-from threading import Thread
 from src.services import *
+from pdfdocument.document import PDFDocument
+import os
 
 logging.basicConfig(level=logging.INFO)
+
 
 def register_routes(app, cover_letter_generator):
     @app.route("/")
     def index():
         return redirect(url_for("set_keys"))
-    
-    
+
     @app.route("/job_details_page", methods=["GET"])
     def job_details_page():
-        # handler logic here
         return render_template("job_details_page.html")
-    
-        
+
     @app.route("/scrape_linkedin", methods=["POST"])
     def scrape_linkedin():
         linkedin_url = request.form.get("linkedin_url")
-
-        # Get the HTML content of the LinkedIn job posting page
-        html_doc = get_html_from_url(linkedin_url)
-
-        # Scrape the job posting
-        job_title, company_name, job_description = scrape_linkedin_job_posting(html_doc)
+        job_title, company_name, job_description = get_linkedin_job_details(
+            linkedin_url
+        )
 
         # Return the scraped data as a JSON response
         return {
             "job_title": job_title,
             "company_name": company_name,
-            "job_description": job_description
+            "job_description": job_description,
         }
 
     @app.route("/generate_cover_letter", methods=["POST"])
     def generate_cover_letter():
-        with cover_letter_generator.lock:
-            if cover_letter_generator.is_running:
-                return {"status": "A cover letter generation is already in progress."}, 409
-        logging.info("Generating cover letter.")
         company_name = request.form.get("company_name")
         position = request.form.get("position")
         job_descript = request.form.get("job_descript")
+        is_running, response, status_code = generate_cover_letter_async(
+            cover_letter_generator, company_name, position, job_descript
+        )
+        if is_running:
+            return response, status_code
+        else:
+            return response, status_code
 
-        Thread(
-            target=cover_letter_generator.query, args=(company_name, position, job_descript)
-        ).start()
-
-        return {"status": "Generating cover letter..."}, 202
-        # cover_letter = cover_letter_generator.query(company_name, position, job_descript)
-        # save_cover_letter_to_file(cover_letter)
-        # return {"cover_letter": cover_letter}
-
-
-    @app.route('/get_final_cover_letter', methods=['GET'])
+    @app.route("/get_final_cover_letter", methods=["GET"])
     def get_final_cover_letter():
         with cover_letter_generator.lock:
             if cover_letter_generator.is_running:
                 return {"status": "A cover letter generation is in progress."}, 409
         cover_letter_final = cover_letter_generator.query_final()
-        save_cover_letter_to_file(cover_letter_final, cover_letter_generator.company_name)
-        
+        save_cover_letter_to_file(
+            cover_letter_final, cover_letter_generator.company_name
+        )
+
         return jsonify({"cover_letter": cover_letter_final})
-    
-    
+
     @app.route("/generate_cover_letter", methods=["GET"])
     def stream_generate_cover_letter():
         # cover_letter_generator.reset()
@@ -85,108 +75,76 @@ def register_routes(app, cover_letter_generator):
     @app.route("/set_keys", methods=["GET", "POST"])
     def set_keys():
         # Location of the secrets file
-    
+
         # If the secrets file already exists and the keys are set, redirect to upload page
         if are_keys_set():
             return redirect(url_for("upload_page"))
-    
+
         if request.method == "POST":
             new_openai_api_key = request.form.get("openai_api_key")
             new_google_api_key = request.form.get("google_api_key")
             new_google_cse_id = request.form.get("google_cse_id")
-    
+
             # Call the function to update the keys in the secrets.ini file
             update_secrets_in_file(
                 openai_api_key=new_openai_api_key,
                 google_api_key=new_google_api_key,
                 google_cse_id=new_google_cse_id,
             )
-    
+
             return redirect(url_for("upload_page"))
         else:
             return render_template("set_keys.html")
-        
+
     @app.route("/upload_page", methods=["GET"])
     def upload_page():
         logging.info("IN PAGE.")
         return render_template("upload_files.html")
-    
-    
+
     @app.route("/upload_resume", methods=["POST"])
     def upload_resume():
         logging.info("In upload_resume!")
         if "file" in request.files:
-            resume_file = request.files["file"]
-            if allowed_file(resume_file.filename):
-                _, file_extension = os.path.splitext(
-                    resume_file.filename
-                )  # Get file extension
-                filename = "resume" + file_extension  # Create new filename
-                resume_file_name = secure_filename(filename)
-                file_path = os.path.join("Data", resume_file_name)
-                logging.info(f"Saving file to: {file_path}")
-                resume_file.save(file_path)
-                cover_letter_generator.load_documents()
-    
-        return "", 200
-    
-    
+            save_uploaded_file(cover_letter_generator, request.files["file"], "resume")
+            return "", 200
+
     @app.route("/upload_cover_letter", methods=["POST"])
     def upload_cover_letter():
         logging.info("In upload_cover_letter!")
         if "file" in request.files:
-            cover_letter_file = request.files["file"]
-            if allowed_file(cover_letter_file.filename):
-                _, file_extension = os.path.splitext(
-                    cover_letter_file.filename
-                )  # Get file extension
-                filename = "cover_letter" + file_extension  # Create new filename
-                cover_file_name = secure_filename(filename)
-                file_path = os.path.join("Data", cover_file_name)
-                logging.info(f"Saving file to: {file_path}")
-                cover_letter_file.save(file_path)
-                cover_letter_generator.load_documents()
-        return "", 200
-    
-    
+            save_uploaded_file(
+                cover_letter_generator, request.files["file"], "cover_letter"
+            )
+            return "", 200
+
     @app.route("/upload_other", methods=["POST"])
     def upload_other():
         logging.info("In upload_other!")
         if "file" in request.files:
-            other_file = request.files["file"]
-            if allowed_file(other_file.filename):
-                _, file_extension = os.path.splitext(
-                    other_file.filename
-                )  # Get file extension
-                filename = "other" + file_extension  # Create new filename
-                other_file_name = secure_filename(filename)
-                file_path = os.path.join("Data", other_file_name)
-                logging.info(f"Saving file to: {file_path}")
-                other_file.save(file_path)
-                cover_letter_generator.load_documents()
-        return "", 200
-    
-    
-    @app.route('/download', methods=['GET'])
+            save_uploaded_file(cover_letter_generator, request.files["file"], "other")
+            return "", 200
+
+    @app.route("/download", methods=["GET"])
     def download():
-        company_name = cover_letter_generator.company_name.replace(" ", "_")
-        txt_filename = f"generated_cover_letters/txt/{company_name}_cover_letter.txt"
-        pdf_filename = f"generated_cover_letters/pdf/{company_name}_cover_letter.pdf"
+        txt_filename = generate_filename(cover_letter_generator.company_name, "txt")
+        pdf_filename = generate_filename(cover_letter_generator.company_name, "pdf")
 
-        # Open the cover letter text file
-        with open(txt_filename, 'r') as f:
-            text = f.read()
+        # Check if the text file exists
+        if not file_exists(txt_filename):
+            return "Error: text file not found"
 
-        pdf = PDFDocument(pdf_filename)
-        pdf.init_report()
-        pdf.generate_style(font_size=11, font_name='Times-Roman')
-        pdf.p(text)
-        pdf.generate()
+        # Read the text from the text file
+        text = read_text_file(txt_filename)
 
-        # # Check if the PDF was created successfully
-        if not os.path.exists(pdf_filename):
+        # Create a PDF from the text
+        create_pdf_from_text(text, pdf_filename)
+
+        # Check if the PDF was created successfully
+        if not file_exists(pdf_filename):
             return "Error: could not create PDF"
-        cover_letter_generator.reset()  # re-initialize the cover letter generator
+
+        # re-initialize the cover letter generator
+        reset_generator(cover_letter_generator)
+
         # Send the PDF file to the user
         return send_file(pdf_filename, as_attachment=True)
-
--- src/services.py
+++ src/services.py
@@ -6,38 +6,49 @@ import requests
 from bs4 import BeautifulSoup
 import json
 import html
+from threading import Thread
+
+from pdfdocument.document import PDFDocument
+
+# Get the directory of the current script
+script_dir = os.path.dirname(os.path.realpath(__file__))
+
+# Build the path to the secrets file
+secrets_file = os.path.join(script_dir, "../secrets.ini")
+
 
 def get_html_from_url(url):
     response = requests.get(url)
     response.raise_for_status()  # This will raise an exception if there's an error
     return response.text
 
+
 def scrape_linkedin_job_posting(html_doc):
-    soup = BeautifulSoup(html_doc, 'html.parser')
+    soup = BeautifulSoup(html_doc, "html.parser")
 
     # Extract job title from the 'og:title' meta tag
-    og_title = soup.find('meta', property='og:title')['content']
+    og_title = soup.find("meta", property="og:title")["content"]
 
     # Split the 'og:title' content by ' hiring '
-    split_title = og_title.split(' hiring ')
+    split_title = og_title.split(" hiring ")
 
     # The first part is the company name
     company_name = split_title[0]
 
     # The second part contains the job title and location, split it by ' in '
-    job_title_location = split_title[1].split(' in ')
+    job_title_location = split_title[1].split(" in ")
 
     # The first part is the job title
     job_title = job_title_location[0]
 
     # Extract job description from the 'JobPosting' schema
-    job_posting_schema = soup.find('script', type='application/ld+json')
+    job_posting_schema = soup.find("script", type="application/ld+json")
     job_posting_data = json.loads(job_posting_schema.string)
-    job_description_html = job_posting_data['description']
+    job_description_html = job_posting_data["description"]
 
     # Convert HTML to plain text
-    job_description_soup = BeautifulSoup(job_description_html, 'html.parser')
-    job_description = job_description_soup.get_text(separator='\n')
+    job_description_soup = BeautifulSoup(job_description_html, "html.parser")
+    job_description = job_description_soup.get_text(separator="\n")
 
     # Unescape HTML entities
     job_description = html.unescape(job_description)
@@ -49,7 +60,7 @@ def update_secrets_in_file(
     openai_api_key=None, google_api_key=None, google_cse_id=None
 ):
     config = configparser.ConfigParser()
-    config.read("../secrets.ini")
+    config.read(secrets_file)
 
     if openai_api_key:
         config["DEFAULT"]["OPENAI_API_KEY"] = openai_api_key
@@ -60,19 +71,17 @@ def update_secrets_in_file(
     if google_cse_id:
         config["DEFAULT"]["GOOGLE_CSE_ID"] = google_cse_id
 
-    with open("secrets.ini", "w") as configfile:
+    with open(secrets_file, "w") as configfile:
         config.write(configfile)
 
 
 def are_keys_set():
-    secrets_file = "secrets.ini"
     config = configparser.ConfigParser()
     config.read(secrets_file)
     return all(val for val in config["DEFAULT"].values())
 
 
 def allowed_file(filename):
-
     ALLOWED_EXTENSIONS = {"txt", "pdf", "docx"}
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
@@ -80,5 +89,72 @@ def allowed_file(filename):
 def save_cover_letter_to_file(cover_letter, company_name):
     txt_filename = f"generated_cover_letters/txt/{company_name}_cover_letter.txt"
     # Save the cover letter as a text file
-    with open(txt_filename, 'w') as file:
+    with open(txt_filename, "w") as file:
         file.write(cover_letter)
+
+
+def save_uploaded_file(cover_letter_generator, file, filename_prefix):
+    if allowed_file(file.filename):
+        _, file_extension = os.path.splitext(file.filename)
+        filename = filename_prefix + file_extension
+        secure_file_name = secure_filename(filename)
+        file_path = os.path.join("Data", secure_file_name)
+        logging.info(f"Saving file to: {file_path}")
+        file.save(file_path)
+        cover_letter_generator.load_documents()
+
+
+def get_linkedin_job_details(url):
+    # Get the HTML content of the LinkedIn job posting page
+    html_doc = get_html_from_url(url)
+
+    # Scrape the job posting
+    return scrape_linkedin_job_posting(html_doc)
+
+
+def generate_cover_letter_async(
+    cover_letter_generator, company_name, position, job_descript
+):
+    with cover_letter_generator.lock:
+        if cover_letter_generator.is_running:
+            return (
+                True,
+                {"status": "A cover letter generation is already in progress."},
+                409,
+            )
+    logging.info("Generating cover letter.")
+    Thread(
+        target=cover_letter_generator.query,
+        args=(company_name, position, job_descript),
+    ).start()
+    return False, {"status": "Generating cover letter..."}, 202
+
+
+def generate_filename(company_name: str, extension: str) -> str:
+    company_name = company_name.replace(" ", "_")
+    filename = (
+        f"generated_cover_letters/{extension}/{company_name}_cover_letter.{extension}"
+    )
+    return filename
+
+
+def read_text_file(filename: str) -> str:
+    with open(filename, "r") as f:
+        text = f.read()
+    return text
+
+
+def create_pdf_from_text(text: str, pdf_filename: str) -> None:
+    pdf = PDFDocument(pdf_filename)
+    pdf.init_report()
+    pdf.generate_style(font_size=11, font_name="Times-Roman")
+    pdf.p(text)
+    pdf.generate()
+
+
+def file_exists(filename: str) -> bool:
+    return os.path.exists(filename)
+
+
+def reset_generator(cover_letter_generator):
+    cover_letter_generator.reset()
